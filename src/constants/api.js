5ab00cbba83aae20c1c92d0f08add209ea833592
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2023-11-09 18:47:55 +0530

    api.js is updated

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,14 @@
-export const userAPI = `http://localhost:8000/accounts`
-export const chatAPI = `http://localhost:8000/chat`
-export const adminAPI = `http://localhost:8000/dashboard/`
-export const trainerAPI = `http://localhost:8000/trainer/`
+// export const userAPI = `http://localhost:8000/accounts`
+// export const chatAPI = `http://localhost:8000/chat`
+// export const adminAPI = `http://localhost:8000/dashboard/`
+// export const trainerAPI = `http://localhost:8000/trainer/`
 
-export const wsApiUrl = 'ws://127.0.0.1:8000';
+// export const wsApiUrl = 'ws://127.0.0.1:8000';
+
+
+export const userAPI = `https://talki.onrender.com/accounts`
+export const chatAPI = `https://talki.onrender.com/chat`
+export const adminAPI = `https://talki.onrender.com/dashboard`
+export const trainerAPI = `https://talki.onrender.com/trainer`
+
+export const wsApiUrl = 'wss://talki.onrender.com';
